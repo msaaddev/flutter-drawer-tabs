e720be73130a80449f3bfb531eafa3cfb3bbc6a7
msaaddev <mrsaadirfan@gmail.com>
2021-05-31 23:06:12 +0500

    👌 IMPROVE: five different buttons

--- lib/screens/FiveTab.dart
+++ lib/screens/FiveTab.dart
@@ -8,39 +8,111 @@ class FiveTab extends StatefulWidget {
   FiveTabState createState() => FiveTabState();
 }
 
-class FiveTabState extends State<FiveTab> {
+enum Alphabets { A, B, C, D }
+
+class FiveTabState extends State<FiveTab> with TickerProviderStateMixin {
+  double _volume = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Five Tab Screen',
         home: DefaultTabController(
-          length: 5,
-          child: Scaffold(
-            drawer: DrawerW(),
-            appBar: AppBar(
-              title: Text('Five Tab Screen'),
-              bottom: TabBar(
-                tabs: [
-                  Tab(
-                    text: 'Tab1',
-                  ),
-                  Tab(
-                    text: 'Tab2',
-                  ),
-                  Tab(
-                    text: 'Tab3',
-                  ),
-                  Tab(
-                    text: 'Tab4',
-                  ),
-                  Tab(
-                    text: 'Tab5',
-                  )
-                ],
+            length: 5,
+            child: Scaffold(
+              drawer: DrawerW(),
+              appBar: AppBar(
+                title: Text('Five Tab Screen'),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(
+                      text: 'Elevated',
+                    ),
+                    Tab(
+                      text: 'Floating',
+                    ),
+                    Tab(
+                      text: 'Icon Btn',
+                    ),
+                    Tab(
+                      text: 'Outline',
+                    ),
+                    Tab(
+                      text: 'Popup Menu',
+                    )
+                  ],
+                ),
               ),
-            ),
-            body: Container(),
-          ),
-        ));
+              body: Center(
+                  child: Container(
+                width: 250,
+                height: 100,
+                child: TabBarView(children: [
+                  ElevatedButton(
+                    child: Text(
+                      'Elevated Button',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueAccent[700],
+                      onPrimary: Colors.white,
+                      elevation: 50,
+                      shadowColor: Colors.blueAccent,
+                    ),
+                    onPressed: () {},
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(Icons.tab),
+                    backgroundColor: Colors.blue,
+                  ),
+                  Column(children: [
+                    IconButton(
+                      icon: Icon(Icons.volume_up),
+                      tooltip: 'Increase volume by 10',
+                      onPressed: () {
+                        setState(() {
+                          _volume += 10;
+                        });
+                      },
+                    ),
+                    Text('Volume : $_volume')
+                  ]),
+                  OutlinedButton(
+                    child: Text(
+                      'Outline Button',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                  PopupMenuButton<Alphabets>(
+                    onSelected: (Alphabets result) {},
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<Alphabets>>[
+                      const PopupMenuItem<Alphabets>(
+                        value: Alphabets.A,
+                        child: Text('Alphabel A'),
+                      ),
+                      const PopupMenuItem<Alphabets>(
+                        value: Alphabets.B,
+                        child: Text('Alphabel B'),
+                      ),
+                      const PopupMenuItem<Alphabets>(
+                        value: Alphabets.C,
+                        child: Text('Alphabel C'),
+                      ),
+                      const PopupMenuItem<Alphabets>(
+                        value: Alphabets.D,
+                        child: Text('Alphabel D'),
+                      ),
+                    ],
+                  ),
+                ]),
+              )),
+            )));
   }
 }
